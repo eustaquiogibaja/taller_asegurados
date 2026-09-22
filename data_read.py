572ef7a882b5a5a8299f06a7338d432132e8e5c8
eustaquiogibaja <122296238+eustaquiogibaja@users.noreply.github.com>
2026-09-21 21:37:06 -0500

    Update data_read.py
    cambio de urgencia

--- data_read.py
+++ data_read.py
@@ -1,14 +1,14 @@
 import pandas as pd
 
 # Ruta del archivo TXT
-archivo_txt = 'Asegurados.txt'
+archivo_excel = 'Asegurados.csv'
 
 try:
     # Leer el archivo TXT
     data = pd.read_csv(archivo_txt, delimiter='|')
     
     # Guardar los datos en un archivo CSV temporal
-    archivo_csv = 'asegurados_certus.csv'
+    archivo_excel = 'asegurados.csv'
     data.to_csv(archivo_csv, index=False)
     
     print(f"Datos leídos y guardados en {archivo_csv}")
