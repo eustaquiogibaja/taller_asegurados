58f29b86494395feee2c6eb857845c360450a51f
eustaquiogibaja <122296238+eustaquiogibaja@users.noreply.github.com>
2026-09-21 21:27:12 -0500

    Update data_read.py
    cambios de emergencia para dataset.

--- data_read.py
+++ data_read.py
@@ -1,14 +1,14 @@
 import pandas as pd
 
 # Ruta del archivo TXT
-archivo_txt = 'Claro_Peru_Asegurados_20022021234238.txt'
+archivo_txt = 'Asegurados.txt'
 
 try:
     # Leer el archivo TXT
     data = pd.read_csv(archivo_txt, delimiter='|')
     
     # Guardar los datos en un archivo CSV temporal
-    archivo_csv = 'clientes.csv'
+    archivo_csv = 'asegurados_certus.csv'
     data.to_csv(archivo_csv, index=False)
     
     print(f"Datos leídos y guardados en {archivo_csv}")
